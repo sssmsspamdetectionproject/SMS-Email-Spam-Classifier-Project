6231dbd06b6b870c1cc75ad928b74b04cd6ead7d
sssmsspamdetectionproject <143194928+sssmsspamdetectionproject@users.noreply.github.com>
2023-08-25 23:24:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-nltk.download('punkt')
-nltk.download('stopwords')
 import streamlit as st
 import pickle
 import string
 from nltk.corpus import stopwords
 import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
 from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
