98f0961197a8507e4ded0e2253d10bffd55caf37
SMS SPAM CLASSIFIER <143194928+sssmsspamdetectionproject@users.noreply.github.com>
2024-08-28 18:15:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from nltk.stem.porter import PorterStemmer
 # Download necessary NLTK resources
 nltk.download('punkt')
 nltk.download('stopwords')
-nltk.download('punkt_tab')
 
 ps = PorterStemmer()
 
